b9fa9b0bdb28288b3a5eb42682cc4b849cfc7a62
izarin00 <firzamuhammad85@gmail.com>
2022-07-26 13:24:32 +0700

    ujikom2022

---
+++ resources/views/template/nav.blade.php
@@ -0,0 +1,7 @@
+<nav class="nav">
+    <a class="nav-link active" aria-current="page" href="#">MBSshop</a>
+    <a class="nav-link" href="#">home</a>
+    <a class="nav-link" href="#">produk</a>
+    <a class="nav-link disabled"">keranjang</a>
+    <a class="nav-link disabled"">checkout</a>
+  </nav>
